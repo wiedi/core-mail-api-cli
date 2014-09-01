c5a357e54913de92ee8d086b6450184d2a4e3635
Sebastian Wiedenroth <wiedi@frubar.net>
2014-09-02 00:00:21 +0200

    add catch-all tests

--- test.py
+++ test.py
@@ -7,56 +7,55 @@ import unittest
 import random
 import string
 
-
+api = MailAPI(CONFIG['api_server'],
+	username = CONFIG['api_username'],
+	password = CONFIG['api_password'],
+	verify   = CONFIG['api_verify_ssl'],
+)
 
 PASS = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20))
 
+def createBasicLayout():
+	api.createMailAccount('acc1', CONFIG['test_domain'], PASS)
+	api.createMailAccount('acc2', CONFIG['test_domain'], PASS)
+	api.createMailAccount('acc3', CONFIG['test_domain'], PASS)
+
+	api.createMailAccount('acc-and-alias', CONFIG['test_domain'], PASS)
+
+	api.createMailAlias('alias1', CONFIG['test_domain'], 'acc1@' + CONFIG['test_domain'])
+	api.createMailAlias('alias2', CONFIG['test_domain'], 'acc1@' + CONFIG['test_domain'] + ',acc2@' + CONFIG['test_domain'])
+	api.createMailAlias('alias3', CONFIG['test_domain'],
+		'acc1@'   + CONFIG['test_domain'] + ',' + \
+		'alias2@' + CONFIG['test_domain'] + ',' + \
+		'acc3@'   + CONFIG['test_domain'] + ','
+	)
+
+	external_addresses = ','.join([e['username'] for e in CONFIG['external']])
+	api.createMailAlias('alias4', CONFIG['test_domain'], external_addresses)
+	api.createMailAlias('alias5', CONFIG['test_domain'], 'alias5@' + CONFIG['test_domain'])
+
+	api.createMailAlias('acc-and-alias', CONFIG['test_domain'], 'acc1@' + CONFIG['test_domain'])
+
+
+def removeBasicLayout():
+	api.deleteMailAccount('acc1', CONFIG['test_domain'])
+	api.deleteMailAccount('acc2', CONFIG['test_domain'])
+	api.deleteMailAccount('acc3', CONFIG['test_domain'])
+
+	api.deleteMailAlias('alias1', CONFIG['test_domain'])
+	api.deleteMailAlias('alias2', CONFIG['test_domain'])
+	api.deleteMailAlias('alias3', CONFIG['test_domain'])
+	api.deleteMailAlias('alias4', CONFIG['test_domain'])
+	api.deleteMailAlias('alias5', CONFIG['test_domain'])
 
 class BasicTests(unittest.TestCase):
 	@classmethod
 	def setUpClass(cls):
-		api = MailAPI(CONFIG['api_server'],
-			username = CONFIG['api_username'],
-			password = CONFIG['api_password'],
-			verify   = CONFIG['api_verify_ssl'],
-		)
-		api.createMailAccount('acc1', CONFIG['test_domain'], PASS)
-		api.createMailAccount('acc2', CONFIG['test_domain'], PASS)
-		api.createMailAccount('acc3', CONFIG['test_domain'], PASS)
-		
-		api.createMailAccount('acc-and-alias', CONFIG['test_domain'], PASS)
-		
-		api.createMailAlias('alias1', CONFIG['test_domain'], 'acc1@' + CONFIG['test_domain'])
-		api.createMailAlias('alias2', CONFIG['test_domain'], 'acc1@' + CONFIG['test_domain'] + ',acc2@' + CONFIG['test_domain'])
-		api.createMailAlias('alias3', CONFIG['test_domain'],
-			'acc1@'   + CONFIG['test_domain'] + ',' + \
-			'alias2@' + CONFIG['test_domain'] + ',' + \
-			'acc3@'   + CONFIG['test_domain'] + ','
-		)
-		
-		external_addresses = ','.join([e['username'] for e in CONFIG['external']])
-		api.createMailAlias('alias4', CONFIG['test_domain'], external_addresses)
-		api.createMailAlias('alias5', CONFIG['test_domain'], 'alias5@' + CONFIG['test_domain'])
-		
-		api.createMailAlias('acc-and-alias', CONFIG['test_domain'], 'acc1@' + CONFIG['test_domain'])
+		createBasicLayout()
 
 	@classmethod
 	def tearDownClass(cls):
-		api = MailAPI(CONFIG['api_server'],
-			username = CONFIG['api_username'],
-			password = CONFIG['api_password'],
-			verify   = CONFIG['api_verify_ssl'],
-		)
-		api.deleteMailAccount('acc1', CONFIG['test_domain'])
-		api.deleteMailAccount('acc2', CONFIG['test_domain'])
-		api.deleteMailAccount('acc3', CONFIG['test_domain'])
-
-		api.deleteMailAlias('alias1', CONFIG['test_domain'])
-		api.deleteMailAlias('alias2', CONFIG['test_domain'])
-		api.deleteMailAlias('alias3', CONFIG['test_domain'])
-		api.deleteMailAlias('alias4', CONFIG['test_domain'])
-		api.deleteMailAlias('alias5', CONFIG['test_domain'])
-
+		removeBasicLayout()
 
 	def test_send_to_account(self):
 		token = mail.send(
@@ -99,7 +98,7 @@ class BasicTests(unittest.TestCase):
 			debug    = CONFIG['debug'],
 			ssl      = True,
 		)
-	
+
 	def test_send_to_many(self):
 		token = mail.send(
 			to        = ['alias1@' + CONFIG['test_domain'], 'acc2@' + CONFIG['test_domain']],
@@ -129,7 +128,7 @@ class BasicTests(unittest.TestCase):
 			debug    = CONFIG['debug'],
 			ssl      = True,
 		)
-	
+
 	def test_send_to_alias_with_mulitple_destinations(self):
 		token = mail.send(
 			to        = ['alias2@' + CONFIG['test_domain']],
@@ -189,8 +188,6 @@ class BasicTests(unittest.TestCase):
 			debug    = CONFIG['debug'],
 			ssl      = True,
 		)
-
-
 
 	def test_send_indirect_double_via_alias(self):
 		token = mail.send(
@@ -265,6 +262,114 @@ class BasicTests(unittest.TestCase):
 			ssl       = True
 		)
 
+
+class CatchAllTests(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		createBasicLayout()
+		api.createMailAlias('*', CONFIG['test_domain'], 'acc2@' + CONFIG['test_domain'])
+
+	@classmethod
+	def tearDownClass(cls):
+		removeBasicLayout()
+		api.deleteMailAlias('*', CONFIG['test_domain'])
+
+	def test_basic_account(self):
+		# send mail directly to account, should still work
+		token = mail.send(
+			to        = ['acc1@' + CONFIG['test_domain']],
+			from_addr = 'acc1@' + CONFIG['test_domain'],
+			server    = CONFIG['submission_server'],
+			port      = '587',
+			user      = 'acc1@' + CONFIG['test_domain'],
+			password  = PASS,
+			debug     = CONFIG['debug'],
+			ssl       = True
+		)
+		mail.waitformail(
+			token    = token,
+			server   = CONFIG['mailbox_server'],
+			port     = "993",
+			user     = 'acc1@' + CONFIG['test_domain'],
+			password = PASS,
+			debug    = CONFIG['debug'],
+			ssl      = True,
+		)
+
+	def test_basic_alias(self):
+		# send mail to existing alias, should also still work
+		token = mail.send(
+			to        = ['alias1@' + CONFIG['test_domain']],
+			from_addr = 'acc1@' + CONFIG['test_domain'],
+			server    = CONFIG['submission_server'],
+			port      = '587',
+			user      = 'acc1@' + CONFIG['test_domain'],
+			password  = PASS,
+			debug     = CONFIG['debug'],
+			ssl       = True
+		)
+		mail.waitformail(
+			token    = token,
+			server   = CONFIG['mailbox_server'],
+			port     = "993",
+			user     = 'acc1@' + CONFIG['test_domain'],
+			password = PASS,
+			debug    = CONFIG['debug'],
+			ssl      = True,
+		)
+
+	def test_acc_and_alias(self):
+		# send to acc-and-alias
+		token = mail.send(
+			to        = ['acc-and-alias@' + CONFIG['test_domain']],
+			from_addr = 'acc1@' + CONFIG['test_domain'],
+			server    = CONFIG['submission_server'],
+			port      = '587',
+			user      = 'acc1@' + CONFIG['test_domain'],
+			password  = PASS,
+			debug     = CONFIG['debug'],
+			ssl       = True
+		)
+		mail.waitformail(
+			token    = token,
+			server   = CONFIG['mailbox_server'],
+			port     = "993",
+			user     = 'acc1@' + CONFIG['test_domain'],
+			password = PASS,
+			debug    = CONFIG['debug'],
+			ssl      = True,
+		)
+		mail.waitformail(
+			token    = token,
+			server   = CONFIG['mailbox_server'],
+			port     = "993",
+			user     = 'acc-and-alias@' + CONFIG['test_domain'],
+			password = PASS,
+			debug    = CONFIG['debug'],
+			ssl      = True,
+		)
+
+	def test_catchall(self):
+		token = mail.send(
+			to        = ['does-not-exist@' + CONFIG['test_domain']],
+			from_addr = 'acc1@' + CONFIG['test_domain'],
+			server    = CONFIG['submission_server'],
+			port      = '587',
+			user      = 'acc1@' + CONFIG['test_domain'],
+			password  = PASS,
+			debug     = CONFIG['debug'],
+			ssl       = True
+		)
+		mail.waitformail(
+			token    = token,
+			server   = CONFIG['mailbox_server'],
+			port     = "993",
+			user     = 'acc2@' + CONFIG['test_domain'],
+			password = PASS,
+			debug    = CONFIG['debug'],
+			ssl      = True,
+		)
+
+
 if __name__ == '__main__':
-	suite = unittest.TestLoader().loadTestsFromTestCase(BasicTests)
-	unittest.TextTestRunner(verbosity=2).run(suite)
+	unittest.main(verbosity=2)
